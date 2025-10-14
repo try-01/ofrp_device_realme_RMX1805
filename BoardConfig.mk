16a4e9aeab7c18311d929f74266cb0b5d671ba4a
try-01 <142565956+try-01@users.noreply.github.com>
2025-10-14 09:07:56 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,8 @@ TW_INCLUDE_CRYPTO := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 BOARD_USES_QCOM_DECRYPTION := true
-TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_CRYPTO_SYSTEM_VOLD_MOUNT := /vendor /system
 
 # FPS
 TW_FRAMERATE := 60
