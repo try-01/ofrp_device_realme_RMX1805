749fd00c11e5e928eac1e63e1ef1f257e775c555
try-01 <142565956+try-01@users.noreply.github.com>
2025-11-06 09:08:38 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),RMX1805)
+ifneq ($(filter RMX1805,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
