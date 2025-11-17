e8c7981ae97b3a593b0c0937f1bd0f7ed7431cef
try-01 <142565956+try-01@users.noreply.github.com>
2025-11-17 13:03:07 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,12 +115,12 @@ TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 #BOARD_USES_QCOM_DECRYPTION := true
-TW_CRYPTO_USE_SYSTEM_VOLD := \
-    hwservicemanager \
-    keymaster-3-0-qti \
-    qseecomd \
-    servicemanager
-TW_CRYPTO_SYSTEM_VOLD_MOUNT := vendor
+#TW_CRYPTO_USE_SYSTEM_VOLD := \
+#    hwservicemanager \
+#    keymaster-3-0-qti \
+#    qseecomd \
+#    servicemanager
+#TW_CRYPTO_SYSTEM_VOLD_MOUNT := vendor
 
 # FPS
 TW_FRAMERATE := 60
